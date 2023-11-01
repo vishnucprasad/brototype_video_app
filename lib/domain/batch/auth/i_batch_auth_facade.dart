30665381b840b435cd221aea5f8e55a83d4600e1
vishnucprasad <mail@vishnucprasad.in>
2023-11-01 20:20:36 +0530

    Setup auth facade interface for batch in domain layer

---
+++ lib/domain/batch/auth/i_batch_auth_facade.dart
@@ -0,0 +1,22 @@
+import 'package:brototype_video_app/domain/batch/auth/batch_credentials.dart';
+import 'package:brototype_video_app/domain/batch/batch.dart';
+import 'package:brototype_video_app/domain/core/failure.dart';
+import 'package:brototype_video_app/domain/core/tokens.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class IBatchAuthFacade {
+  Future<Either<Failure, Tokens>> login({
+    required BatchCredentials credentials,
+  });
+  Future<void> saveTokens({
+    required Tokens tokens,
+  });
+  Future<Option<Tokens>> getSavedTokens();
+  Future<void> removeTokens();
+  Future<Either<Failure, Batch>> authenticate();
+  Future<Either<Failure, Batch>> getBatchDetails({
+    required String batchId,
+  });
+  Future<Either<Failure, Tokens>> refreshToken();
+  Future<Either<Failure, Unit>> logout();
+}
