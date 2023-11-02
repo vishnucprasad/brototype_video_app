bdd6fb33ee75d72bc2b50d2e8fca0ed40d750eb7
vishnucprasad <mail@vishnucprasad.in>
2023-11-02 21:13:15 +0530

    Disabled screen capturing in Android and IOS

--- android/app/src/main/kotlin/com/vishnucprasad/brototypeworkshop/MainActivity.kt
+++ android/app/src/main/kotlin/com/vishnucprasad/brototypeworkshop/MainActivity.kt
@@ -1,6 +1,13 @@
 package com.vishnucprasad.brototypeworkshop
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+import android.view.WindowManager.LayoutParams
 
-class MainActivity: FlutterActivity() {
+class MainActivity: FlutterFragmentActivity() {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        window.addFlags(LayoutParams.FLAG_SECURE)
+        GeneratedPluginRegistrant.registerWith(flutterEngine)
+    }
 }
